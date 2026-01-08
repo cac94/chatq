fac0509a3b1fe538f4aa531c5ff5cf227aafbaaa
cac94 <cac94@naver.com>
2026-01-08 15:13:45 +0900

    table_metab

--- chatq-server/src/main/java/kr/chatq/server/chatq_server/service/DbService.java
+++ chatq-server/src/main/java/kr/chatq/server/chatq_server/service/DbService.java
@@ -10,10 +10,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
+import org.springframework.beans.factory.annotation.Value;
+
 import kr.chatq.server.chatq_server.config.CompanyContext;
 
 @Service
 public class DbService {
+    private static final String ENC_SEED = "chatq!2015";
 
     private static final Logger logger = LoggerFactory.getLogger(DbService.class);
     private JdbcTemplate jdbcTemplate;
@@ -48,16 +51,23 @@ public class DbService {
         String sql;
         Object[] params;
 
+        StringBuilder sb = new StringBuilder();
         if (auth == null || auth.isEmpty()) {
             // auth가 비어있으면 모든 auth에 대해 조회
-            sql = "SELECT a.* FROM chatqtable a WHERE a.company = ? and a.meta_yn = ?";
+            sb.append("SELECT company, table_nm, table_alias, detail_yn, meta_yn, tail_query, change_date, change_time, change_user, keywords, ");
+            sb.append("CONVERT(AES_DECRYPT(table_metab, '" + ENC_SEED + "') USING utf8mb4) AS table_query ");
+            sb.append(" FROM chatqtable WHERE company = ? and meta_yn = ?");
+            sql = sb.toString();
             params = new Object[]{company, metaYn};
             logger.info("Executing SQL: {} with params: [{}, {}, {}]", sql, company, company, metaYn);
             System.out.println("[DbService] Executing SQL: " + sql + " with params: [" + company + ", " + company + ", "
                     + metaYn + "]");
         } else {
             // auth가 있으면 특정 auth만 조회
-            sql = "SELECT a.* FROM chatqtable a INNER JOIN chatqauth b ON (a.table_nm = b.table_nm) WHERE a.company = ? AND b.company = ? AND b.auth = ? and a.meta_yn = ?";
+            sb.append("SELECT a.company, a.table_nm, a.table_alias, a.detail_yn, a.meta_yn, a.tail_query, a.change_date, a.change_time, a.change_user, a.keywords, ");
+            sb.append("CONVERT(AES_DECRYPT(a.table_metab, '" + ENC_SEED + "') USING utf8mb4) AS table_query ");
+            sb.append("FROM chatqtable a INNER JOIN chatqauth b ON (a.table_nm = b.table_nm) WHERE a.company = ? AND b.company = ? AND b.auth = ? and a.meta_yn = ?");
+            sql = sb.toString();
             params = new Object[]{company, company, auth, metaYn};
             logger.info("Executing SQL: {} with params: [{}, {}, {}, {}]", sql, company, company, auth, metaYn);
             System.out.println("[DbService] Executing SQL: " + sql + " with params: [" + company + ", " + company + ", "
